7e0d69c832c9a72873061f196f92902ee467bc1d
bitzsalex <bitzsalex@gmail.com>
2024-06-06 20:18:31 +0200

    EX: Complete C_00 ex02

---
+++ C_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void ft_print_reverse_alphabet(void);
+
+void ft_print_reverse_alphabet(void) {
+    // the same ASCII conversion will happen
+    // as we saw in the previous exercise
+    int iterator = 'z';
+
+    while (iterator >= 'a') {
+        write(1, &iterator, 1);
+        iterator--;
+    }
+}
+
+int main() {
+    ft_print_reverse_alphabet();
+    return 0;
+}
