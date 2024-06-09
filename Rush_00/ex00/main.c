48d7daccdc77d176a58092c218aa5b77bb95efdc
bitzsalex <bitzsalex@gmail.com>
2024-06-09 17:06:00 +0200

    Ex: Add multiple solution to rush04

--- Rush_00/ex00/main.c
+++ Rush_00/ex00/main.c
@@ -1,19 +1,49 @@
 void rush(int x, int y);
+void rushD(int x, int y);
+void rushF(int x, int y);
 void ft_putchar(char ch);
 
 int main(int argc, char *argv[]) {
     // main subject
-    rush(5, 3);
+    // rush(5, 3);
+    // ft_putchar('\n');
+    // rush(5, 1);
+    // ft_putchar('\n');
+    // rush(1, 1);
+    // ft_putchar('\n');
+    // rush(1, 5);
+    // ft_putchar('\n');
+    // rush(10, 8);
+    // ft_putchar('\n');
+    // rush(123, 42);
+
+    // do...while loop solution
     ft_putchar('\n');
-    rush(5, 1);
+    rushD(5, 3);
     ft_putchar('\n');
-    rush(1, 1);
+    rushD(5, 1);
     ft_putchar('\n');
-    rush(1, 5);
+    rushD(1, 1);
     ft_putchar('\n');
-    rush(10, 8);
+    rushD(1, 5);
     ft_putchar('\n');
-    rush(123, 42);
+    rushD(10, 8);
+    ft_putchar('\n');
+    rushD(123, 42);
+
+    // for loop solution
+    // ft_putchar('\n');
+    // rushF(5, 3);
+    // ft_putchar('\n');
+    // rushF(5, 1);
+    // ft_putchar('\n');
+    // rushF(1, 1);
+    // ft_putchar('\n');
+    // rushF(1, 5);
+    // ft_putchar('\n');
+    // rushF(10, 8);
+    // ft_putchar('\n');
+    // rushF(123, 42);
 
     return 0;
 }
--- Rush_00/ex00/rush04.c
+++ Rush_00/ex00/rush04.c
@@ -1,4 +1,20 @@
 void ft_putchar(char ch);
+char getPrintedChar(int colX, int rowY, int x, int y);
+
+char getPrintedChar(int colX, int rowY, int x, int y) {
+    char printedChar = ' ';
+
+    // check if we are on the first row & first collumn or
+    // on the last row & last column but only the row and column counts are greater than one
+    if ((colX == 1 && rowY == 1) || (colX == x && rowY == y && colX > 1 && rowY > 1)) printedChar = 'A';
+    // check if we are on the first row & last column or
+    // on the last row & first column
+    else if ((colX == x && rowY == 1) || (colX == 1 && rowY == y)) printedChar = 'C';
+    // check if we are on the first and last row, or first and last column
+    else if (colX == 1 || colX == x || rowY == 1 || rowY == y) printedChar = 'B';
+
+    return printedChar;
+}
 
 /**
  * This function is a while loop solution
@@ -9,15 +25,7 @@ void rush(int x, int y) {
 
     while (rowY <= y) {
         while (colX <= x) {
-            printedChar = ' ';
-            // check if we are on the first row & first collumn or
-            // on the last row & last column but only the row and column counts are greater than one
-            if ((colX == 1 && rowY == 1) || (colX == x && rowY == y && colX > 1 && rowY > 1)) printedChar = 'A';
-            // check if we are on the first row & last column or
-            // on the last row & first column
-            else if ((colX == x && rowY == 1) || (colX == 1 && rowY == y)) printedChar = 'C';
-            // check if we are on the first and last row, or first and last column
-            else if (colX == 1 || colX == x || rowY == 1 || rowY == y) printedChar = 'B';
+            printedChar = getPrintedChar(colX, rowY, x, y);
             ft_putchar(printedChar);
             // increment the column counter by 1
             colX++;
@@ -25,6 +33,44 @@ void rush(int x, int y) {
 
         // increment the row counter by 1, and reset the column counter
         rowY++, colX = 1;
+        ft_putchar('\n');
+    }
+}
+
+
+/**
+ * This function is a do...while loop solution
+ */
+void rushD(int x, int y) {
+    int colX = 1, rowY = 1;
+    char printedChar = ' ';
+
+    do {
+        do {
+            printedChar = getPrintedChar(colX, rowY, x, y);
+            ft_putchar(printedChar);
+            colX++;
+        } while (colX <= x);
+
+        // increment the row counter by 1, and reset the column counter
+        rowY++, colX = 1;
+        ft_putchar('\n');
+    } while (rowY <= y);
+}
+
+
+/**
+ * This function is a for loop solution
+ */
+void rushF(int x, int y) {
+    int colX = 1, rowY = 1;
+    char printedChar = ' ';
+
+    for (int rowY = 1; rowY <= y; rowY++) {
+        for (int colX = 1; colX <= x; colX++) {
+            printedChar = getPrintedChar(colX, rowY, x, y);
+            ft_putchar(printedChar);
+        }
         ft_putchar('\n');
     }
 }
