1555034fd8f40e3c3a233417486d106aad367ea6
bitzsalex <bitzsalex@gmail.com>
2024-06-10 15:46:54 +0200

    Bug: Fix C_00 ex08

--- C_00/ex08/ft_print_combn.c
+++ C_00/ex08/ft_print_combn.c
@@ -1,106 +1,62 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int g_digit_count = 10;
 
+int is_last_comb(char *buffer, int end);
 void ft_print_combn(int n);
-int are_all_next9s(int arr[], int length);
-void fill_all(int arr[], int length, int init, int start_index);
-void adjust_increment(int arr[], int length);
+void generate_combination(char *buffer, int index, int start, int end);
 
-int are_all_next9s(int arr[], int length)
+int is_last_comb(char *buffer, int end)
 {
     int itr = 1;
+    char last_digit = (10 - end) + '0';
 
-    while (itr < length)
+    while (itr < end)
     {
-        if (arr[itr] != 9)
-            return (0);
+        if (buffer[itr] != (last_digit + itr))
+            return 0;
+
         itr++;
     }
 
-    return (1);
+    return 1;
 }
 
-void fill_all(int arr[], int length, int init, int start_index)
-{
-    int add = init;
-
-    while (start_index < length)
-    {
-        if (add > 9)
-            break;
-
-        arr[start_index] = add;
-        start_index++;
-        add++;
-    }
-}
-
-void print_combs(int arr[], int length)
+void print_combination(char *buffer, int end)
 {
     int itr = 0;
-    char printed_char = '\0';
     char separator[] = ", ";
+    char last_digit = (g_digit_count - end) + '0';
+    write(1, buffer, end);
 
-    while (itr < length)
-    {
-        printed_char = arr[itr] + '0';
-        write(1, &printed_char, 1);
-        itr++;
-    }
-
-    if (!(arr[0] == (g_digit_count - length) && are_all_next9s(arr, length)))
+    if (!((buffer[0] == last_digit) && is_last_comb(buffer, end)))
         write(1, separator, 2);
 }
 
-void adjust_increment(int arr[], int length)
+void generate_combination(char *buffer, int index, int start, int end)
 {
-    int itr = length - 1;
-
-    while (itr > 0)
+    if (index == end)
     {
-        if (arr[itr] < 9)
-        {
-            fill_all(arr, length, arr[itr] + 1, itr);
-            break;
-        }
-        itr--;
+        print_combination(buffer, end);
+        return;
+    }
+
+    for (int i = start; i <= 9; i++)
+    {
+        buffer[index] = '0' + i;
+        generate_combination(buffer, index + 1, i + 1, end);
     }
 }
 
 void ft_print_combn(int n)
 {
-    int outer = 0;
-    int inner = 0;
-    int digitCombs[g_digit_count]; // the max size
-
-    while (outer <= (g_digit_count - n))
-    {
-        fill_all(digitCombs, n, outer, 0);
-        print_combs(digitCombs, n);
-
-        while (!are_all_next9s(digitCombs, n))
-        {
-            inner = digitCombs[n - 1] + 1;
-
-            while (inner <= 9)
-            {
-                digitCombs[n - 1] = inner;
-                print_combs(digitCombs, n);
-                inner++;
-            }
-
-            adjust_increment(digitCombs, n);
-            print_combs(digitCombs, n);
-        }
-
-        outer++;
-    }
+    char buffer[10];
+    generate_combination(buffer, 0, 0, n);
 }
 
 int main()
 {
-    print_all_combinations(4);
-    // ft_print_combn(9);
+    ft_print_combn(4);
     return 0;
 }
