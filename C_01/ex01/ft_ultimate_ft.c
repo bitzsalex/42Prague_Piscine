d73ebccbac44c567dbc3e43f774b1f93911f0bbd
bitzsalex <bitzsalex@gmail.com>
2024-06-11 00:27:47 +0200

    Ex: Complete C_01 ex01

---
+++ C_01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void ft_ultimate_ft(int *********nbr);
+
+void ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+int main()
+{
+	int num = 10;
+	int *ptr1 = &num;
+	int **ptr2 = &ptr1;
+	int ***ptr3 = &ptr2;
+	int ****ptr4 = &ptr3;
+	int *****ptr5 = &ptr4;
+	int ******ptr6 = &ptr5;
+	int *******ptr7 = &ptr6;
+	int ********ptr8 = &ptr7;
+	int *********ptr9 = &ptr8;
+	printf("before change %d\n", num);
+	ft_ultimate_ft(ptr9);
+	printf("after change %d\n", num);
+	return 0;
+}
