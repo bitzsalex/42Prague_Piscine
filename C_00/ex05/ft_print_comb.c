375149588bb7e6e6395c468c269aac03a09d94e3
bitzsalex <bitzsalex@gmail.com>
2024-06-10 07:00:40 +0200

    Bug: Change other loopings to while

--- C_00/ex05/ft_print_comb.c
+++ C_00/ex05/ft_print_comb.c
@@ -3,18 +3,28 @@
 void ft_print_comb(void);
 
 void ft_print_comb(void) {
-    for (int i = '0'; i <= '7'; i++) {
-        for (int j = i + 1; j <= '8'; j++) {
-            for (int k = j + 1; k <= '9'; k++) {
-                int size = i == '7' && k == '9' ? 3 : 5;
-                char str[size] = {i, j, k};
-                if (!(i == '7' && k == '9')) {
-                    str[3] = ',';
-                    str[4] = ' ';
-                }
-                write(1, str, size);
+    char separator[] = ", ";
+    int outer = 0, inner, deepInner;
+
+    while (outer <= 7) {
+        inner = outer + 1;
+        
+        while (inner <= 8) {
+            deepInner = inner + 1;
+
+            while (deepInner <= 9) {
+                // initialize the string
+                char str[3] = {outer + '0', inner + '0', deepInner + '0'};
+                write(1, str, 3);
+                // print separator if we are not at the end
+                if (!(outer == 7 && deepInner == 9))
+                    write(1, separator, 2);
+
+                deepInner++;
             }
+            inner++;
         }
+        outer++;
     }
 }
 
