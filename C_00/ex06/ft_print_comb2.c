7eacc671df69fd99eacafa67990b5ccc267afc19
bitzsalex <bitzsalex@gmail.com>
2024-06-10 06:34:11 +0200

    Ex: Complete C_00 ex06

--- C_00/ex06/ft_print_comb2.c
+++ C_00/ex06/ft_print_comb2.c
@@ -1,23 +1,39 @@
 #include <unistd.h>
 
 void ft_print_comb2(void);
+void print_comb(int num1, int num2);
+
+void print_comb(int num1, int num2) {
+    int tens1 = num1 / 10, tens2 = num2 / 10;
+    int ones1 = num1 % 10, ones2 = num2 % 10;
+    char str[] = {
+        (tens1 ? tens1 : 0) + '0', ones1 + '0', ' ',
+        (tens2 ? tens2 : 0) + '0', ones2 + '0'
+    };
+    write(1, str, 5);
+}
 
 void ft_print_comb2(void) {
-    for (int i = 0; i <= 1; i++) {
-        int digits = 1;
+    int outer = 0, inner, size;
+    char separater[] = ", ";
 
-        while (digits <= 3) {
-            int counter = 1;
-            int size = digits == 1 ? 1 : (10 - digits) * (digits - 1);
-            
-            while (counter <= size) {
-                 for (int j = 0; j <= 9; j++) {
-                    char str[] = {i + '0', digits + '0', counter + '0', j + '0', ', ', ' '};
-                    write(1, str, 5);
-                }
-                counter++;
-            }
-            digits++;
+    // go until 98
+    while (outer < 99) {
+        // add by 
+        inner = outer + 1;
+
+        while (inner <= 99) {
+            print_comb(outer, inner);
+            if (!(outer == 98 && inner == 99))
+                write(1, separater, 2);
+
+            inner++;
         }
+        outer++;
     }
+}
+
+int main() {
+    ft_print_comb2();
+    return 0;
 }
