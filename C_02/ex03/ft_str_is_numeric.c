c58e907e2948933b6ae692f1b9ba55f038930f73
bitzsalex <bitzsalex@gmail.com>
2024-06-12 00:57:32 +0200

    Ex: Complete C_02 ex03

---
+++ C_02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int ft_str_is_numeric(char *str);
+
+int ft_str_is_numeric(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+int main()
+{
+	printf("is 0123456789 numeric %d\n", ft_str_is_numeric("0123456789"));
+	printf("is abcAbc numeric %d\n", ft_str_is_numeric("abcAbc"));
+	printf("is []@] numeric %d\n", ft_str_is_numeric("[]@]"));
+	printf("is . numeric %d\n", ft_str_is_numeric("."));
+}
