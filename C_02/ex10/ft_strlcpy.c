732715c8204d38fc474ab11efd1d0588ef4ff48b
Betizazu Alemu Desalegn <bdesaleg@c1r10s1.42prague.com>
2024-06-12 13:51:24 +0200

    Ex: Complete C_02 ex10

---
+++ C_02/ex10/ft_strlcpy.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size);
+unsigned int copy_str(char *dest, char *src, unsigned int size, unsigned int counter);
+
+unsigned int copy_str(char *dest, char *src, unsigned int size, unsigned int counter)
+{
+    if (*src == '\0' || size <= 0)
+    {
+		while (*src++ != '\0')
+            counter++;
+        *dest = '\0';
+        return counter;
+    }
+
+    *dest = *src;
+    counter = copy_str(dest + 1, src + 1, size - 1, counter + 1);
+    return (counter);
+}
+
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+    return (copy_str(dest, src, size - 1, 0));
+}
+
+int main()
+{
+    char src[] = "Hello Sir!";
+    char dest[15];
+    int num = ft_strlcpy(dest, src, 12);
+
+    printf("copied size %d\n", num);
+}
