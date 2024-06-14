379409608dbf74490a496583410ebefd1a0157e1
bitzsalex <bitzsalex@gmail.com>
2024-06-14 22:22:17 +0200

    Ex: Complete C_05 ex03

---
+++ C_05/ex03/ft_recursive_power.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdesaleg <marvin@42.ft>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/14 22:21:07 by bdesaleg          #+#    #+#             */
+/*   Updated: 2024/06/14 22:21:10 by bdesaleg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power);
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	else if (power == 0)
+		return (1);
+	return (nb * ft_recursive_power(nb, power - 1));
+}
