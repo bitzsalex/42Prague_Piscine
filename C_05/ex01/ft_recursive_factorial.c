1d43dc2593d8142aa99c265300681eda8e7f4f22
bitzsalex <bitzsalex@gmail.com>
2024-06-14 22:05:23 +0200

    Ex: Complete C_05 ex01

---
+++ C_05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdesaleg <marvin@42.ft>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/14 22:01:11 by bdesaleg          #+#    #+#             */
+/*   Updated: 2024/06/14 22:01:14 by bdesaleg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb);
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
